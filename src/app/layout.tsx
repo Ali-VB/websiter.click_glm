3219bb1778d34116f8125fc588ad0b5b3dcf378a
Ali-Vakili <ali.vakili@example.com>
2025-10-29 14:04:26 -0400

    Fix react-grab script URL to use HTTPS
    - Update script src from protocol-less to HTTPS
    - Ensure reliable loading of react-grab in development mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
       >
         {process.env.NODE_ENV === "development" && (
           <Script
-            src="//unpkg.com/react-grab/dist/index.global.js"
+            src="https://unpkg.com/react-grab/dist/index.global.js"
             crossOrigin="anonymous"
             strategy="beforeInteractive"
             data-enabled="true"
